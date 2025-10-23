19a07d3b5260d4c5877f96786251118b46bf6fc9
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2025-10-23 17:12:13 -0600

    detalles3

--- components/sections/ParentsSection.jsx
+++ components/sections/ParentsSection.jsx
@@ -68,7 +68,7 @@ export default function ParentsSection() {
       ref={sectionRef}
       style={{
         backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('${parents.backgroundImage}')`,
-        backgroundSize: 'cover',
+        backgroundSize: '100% 100%',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
         position: 'relative',
